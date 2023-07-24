1b00e869311fcf3114ad9f33cfbd83fdbd1baded
Vick Aita <75151+vickaita@users.noreply.github.com>
2023-07-24 19:28:55 -0400

    Add more settings
    Add some addition settings from my old config.
    Add vimwiki keybindings

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.relativenumber = false
-vim.opt.number = true
+vim.opt.colorcolumn = "81"
 vim.opt.conceallevel = 0
+vim.opt.listchars = { tab = "▸ ", trail = "•" }
+vim.opt.number = true
+vim.opt.relativenumber = false
--- nvim/.config/nvim/lua/plugins/vimwiki.lua
+++ nvim/.config/nvim/lua/plugins/vimwiki.lua
@@ -1,3 +1,13 @@
 return {
-  { "vimwiki/vimwiki" },
+  {
+    "vimwiki/vimwiki",
+    keys = {
+      { "<leader>vw", "<cmd>VimwikiIndex<cr>", desc = "Vimwiki index" },
+      { "<leader>vdi", "<cmd>VimwikiDiaryIndex<cr>", desc = "Vimwiki diary index" },
+      { "<leader>vdg", "<cmd>VimwikiDiaryGenerateLinks<cr>", desc = "Vimwiki diary generate links" },
+      { "<leader>vdd", "<cmd>VimwikiMakeDiaryNote<cr>", desc = "Vimwiki current date diary note" },
+      { "<leader>vdy", "<cmd>VimwikiMakeYesterdayDiaryNote<cr>", desc = "Vimwiki yesterday diary note" },
+      { "<leader>vdt", "<cmd>VimwikiMakeTomorrowDiaryNote<cr>", desc = "Vimwiki tomrrow diary note" },
+    },
+  },
 }
