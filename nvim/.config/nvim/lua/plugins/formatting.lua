0a2425c68cf0841c8c9a0f3e21ae6a720ce8bcaf
Vick Aita <75151+vickaita@users.noreply.github.com>
2023-08-02 21:58:13 -0400

    Add Python formatting

---
+++ nvim/.config/nvim/lua/plugins/formatting.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      table.insert(opts.ensure_installed, "isort")
+      table.insert(opts.ensure_installed, "black")
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      table.insert(opts.sources, nls.builtins.formatting.isort)
+      table.insert(opts.sources, nls.builtins.formatting.black)
+    end,
+  },
+}
