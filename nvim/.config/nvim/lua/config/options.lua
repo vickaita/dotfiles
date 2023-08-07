bdf88ea87370c2593c4fd24beec74c469e28d3a3
Vick Aita <75151+vickaita@users.noreply.github.com>
2023-08-07 11:34:11 -0400

    Fix clipboard sync behavior

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- don't sync with clipboard by default
+vim.opt.clipboard = "unnamed"
+
 vim.opt.colorcolumn = "81"
 vim.opt.conceallevel = 0
 
