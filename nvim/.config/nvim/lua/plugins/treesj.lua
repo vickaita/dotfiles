b0868321fbf938558afd211898ccb87d38ab49b8
Vick Aita <75151+vickaita@users.noreply.github.com>
2023-08-17 21:56:36 -0400

    Adjust prefix names

--- nvim/.config/nvim/lua/plugins/treesj.lua
+++ nvim/.config/nvim/lua/plugins/treesj.lua
@@ -9,25 +9,29 @@ return {
     end,
     keys = {
       {
+        "<leader>m",
+        desc = "+split/join",
+      },
+      {
         "<leader>mm",
         function()
           require("treesj").toggle()
         end,
-        desc = "Toggle Treesitter Matching",
+        desc = "Toggle Split",
       },
       {
         "<leader>mj",
         function()
           require("treesj").join()
         end,
-        desc = "Join Treesitter Matching",
+        desc = "Join",
       },
       {
         "<leader>ms",
         function()
           require("treesj").split()
         end,
-        desc = "Split Treesitter Matching",
+        desc = "Split",
       },
     },
   },
--- nvim/.config/nvim/lua/plugins/vimwiki.lua
+++ nvim/.config/nvim/lua/plugins/vimwiki.lua
@@ -4,13 +4,17 @@ vim.g.vimwiki_list = { { path = vim.env.VIMWIKI_PATH or "~/wiki/" } }
 return {
   {
     "vimwiki/vimwiki",
+    dependencies = { "folke/which-key.nvim" },
+    ft = { "vimwiki" },
     keys = {
+      { "<leader>v", desc = "+vimwiki" },
+      { "<leader>vd", desc = "+diary" },
       { "<leader>vw", "<cmd>VimwikiIndex<cr>", desc = "Vimwiki index" },
       { "<leader>vdi", "<cmd>VimwikiDiaryIndex<cr>", desc = "Vimwiki diary index" },
       { "<leader>vdg", "<cmd>VimwikiDiaryGenerateLinks<cr>", desc = "Vimwiki diary generate links" },
       { "<leader>vdd", "<cmd>VimwikiMakeDiaryNote<cr>", desc = "Vimwiki current date diary note" },
       { "<leader>vdy", "<cmd>VimwikiMakeYesterdayDiaryNote<cr>", desc = "Vimwiki yesterday diary note" },
-      { "<leader>vdt", "<cmd>VimwikiMakeTomorrowDiaryNote<cr>", desc = "Vimwiki tomrrow diary note" },
+      { "<leader>vdt", "<cmd>VimwikiMakeTomorrowDiaryNote<cr>", desc = "Vimwiki tomorrow diary note" },
     },
   },
 }
