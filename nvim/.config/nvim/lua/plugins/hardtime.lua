31a62236a7d1dace0bbe05b19af98193c23e651f
Vick Aita <75151+vickaita@users.noreply.github.com>
2023-08-23 20:12:18 -0400

    Add keybinding to toggle hardtime

--- nvim/.config/nvim/lua/plugins/hardtime.lua
+++ nvim/.config/nvim/lua/plugins/hardtime.lua
@@ -3,5 +3,8 @@ return {
     "m4xshen/hardtime.nvim",
     dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
     opts = {},
+    keys = {
+      { "<leader>uh", "<cmd>Hardtime toggle<cr>", desc = "Toggle Hardtime" },
+    },
   },
 }
