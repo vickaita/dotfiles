ae6b505d3f017c804ca8e8374e49d376b60e0034
Vick Aita <75151+vickaita@users.noreply.github.com>
2023-08-29 17:20:53 -0400

    Automatically set colorcolumn by textwidth

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -2,13 +2,27 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- set textwidth to 80 in vimwiki files
+-- Bash/Shell
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "bash", "sh" },
+  command = "setlocal shiftwidth=4 tabstop=4",
+})
+
+-- Make
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "make", "Makefile" },
+  command = "setlocal noexpandtab tabstop=8 shiftwidth=8",
+})
+
+-- Vimwiki
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "vimwiki", "wiki" },
   command = "setlocal textwidth=80 shiftwidth=4 tabstop=4",
 })
 
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "bash", "sh" },
-  command = "setlocal shiftwidth=4 tabstop=4",
+-- Automatically set the colorcolumn based on textwidth
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+  callback = function()
+    vim.o.colorcolumn = tostring(vim.o.textwidth + 1)
+  end,
 })
