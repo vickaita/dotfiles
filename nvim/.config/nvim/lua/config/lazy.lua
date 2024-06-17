cf483aca5d1aadcf78c8fbf0d28d55162235977a
Vick Aita <75151+vickaita@users.noreply.github.com>
2024-06-17 10:01:48 -0400

    Update copilot chat integration

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -13,6 +13,7 @@ require("lazy").setup({
 
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.coding.copilot-chat" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
     --
     { import = "lazyvim.plugins.extras.dap.core" },
