8b5a7aa1918c9d91bbb2301509ce90ce777cfa07
Vick Aita <75151+vickaita@users.noreply.github.com>
2025-06-21 11:13:16 -0400

    Improve lesspipe initialization

--- shared/shell/init_lesspipe.sh
+++ shared/shell/init_lesspipe.sh
@@ -1,4 +1,14 @@
 # make less more friendly for non-text input files, see lesspipe(1)
-if [[ -x /usr/bin/lesspipe ]]; then
-    eval "$(SHELL=/bin/sh lesspipe)"
+# Check if 'lesspipe' is available as a command in PATH (most common on Linux)
+if command -v lesspipe >/dev/null 2>&1; then
+  eval "$(SHELL=/bin/sh lesspipe)"
+# Check for lesspipe.sh installed by Homebrew on Apple Silicon Macs
+elif [ -x /opt/homebrew/bin/lesspipe.sh ]; then
+  eval "$(SHELL=/bin/sh /opt/homebrew/bin/lesspipe.sh)"
+# Check for lesspipe.sh installed by Homebrew on Intel Macs
+elif [ -x /usr/local/bin/lesspipe.sh ]; then
+  eval "$(SHELL=/bin/sh /usr/local/bin/lesspipe.sh)"
+# Check for lesspipe.sh in the standard Linux location
+elif [ -x /usr/bin/lesspipe.sh ]; then
+  eval "$(SHELL=/bin/sh /usr/bin/lesspipe.sh)"
 fi
