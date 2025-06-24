eff55c0c8e0d4de5d3e185087a5e141e3de72360
Vick Aita <75151+vickaita@users.noreply.github.com>
2025-06-24 19:55:13 -0400

    Adjust autocmds to add spacing settings for more langs

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,7 +1,7 @@
 -- Bash/Shell
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "bash", "sh" },
-  command = "setlocal shiftwidth=4 tabstop=4",
+  pattern = { "bash", "sh", "zsh" },
+  command = "setlocal shiftwidth=4 tabstop=4 expandtab softtabstop=4",
 })
 
 -- gitcommit
@@ -12,8 +12,8 @@ vim.api.nvim_create_autocmd("FileType", {
 
 -- JavaScript/TypeScript
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "javascript", "typescript" },
-  command = "setlocal textwidth=80 shiftwidth=2 tabstop=2",
+  pattern = { "javascript", "typescript", "javascriptreact", "typescriptreact", "jsx", "tsx" },
+  command = "setlocal textwidth=80 shiftwidth=2 tabstop=2 expandtab softtabstop=2",
 })
 
 -- Make
@@ -25,7 +25,7 @@ vim.api.nvim_create_autocmd("FileType", {
 -- Python
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "python" },
-  command = "setlocal textwidth=79 shiftwidth=4 tabstop=4",
+  command = "setlocal textwidth=79 shiftwidth=4 tabstop=4 expandtab softtabstop=4",
 })
 
 -- Vimwiki
@@ -39,8 +39,122 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
   callback = function()
     -- Try to set the colorcolumn automatically based on textwidth, but only if
     -- textwidth is already set to a value greater than 0
-    if vim.o.textwidth > 0 then
-      vim.o.colorcolumn = tostring(vim.o.textwidth + 1)
+    if vim.opt.textwidth:get() > 0 then
+      vim.opt_local.colorcolumn = tostring(vim.opt.textwidth:get() + 1)
     end
+  end,
+})
+
+-- Web Development
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "css", "scss", "sass", "less" },
+  command = "setlocal shiftwidth=2 tabstop=2 expandtab softtabstop=2",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "html", "xml", "xhtml" },
+  command = "setlocal shiftwidth=2 tabstop=2 expandtab softtabstop=2",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "vue", "svelte" },
+  command = "setlocal shiftwidth=2 tabstop=2 expandtab softtabstop=2",
+})
+
+-- Configuration & Data
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "json", "jsonc" },
+  command = "setlocal shiftwidth=2 tabstop=2 expandtab softtabstop=2",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "yaml", "yml" },
+  command = "setlocal shiftwidth=2 tabstop=2 expandtab softtabstop=2",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "toml" },
+  command = "setlocal shiftwidth=2 tabstop=2 expandtab softtabstop=2",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "dockerfile" },
+  command = "setlocal shiftwidth=2 tabstop=2 expandtab softtabstop=2",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "terraform", "tf" },
+  command = "setlocal shiftwidth=2 tabstop=2 expandtab softtabstop=2",
+})
+
+-- Lua
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "lua" },
+  command = "setlocal shiftwidth=2 tabstop=2 expandtab softtabstop=2",
+})
+
+-- Documentation & Markup
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown", "md" },
+  command = "setlocal spell textwidth=80 shiftwidth=4 tabstop=4 expandtab softtabstop=4",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "rst" },
+  command = "setlocal spell textwidth=79 shiftwidth=3 tabstop=3 expandtab softtabstop=3",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "tex", "latex" },
+  command = "setlocal spell textwidth=80 shiftwidth=2 tabstop=2 expandtab softtabstop=2",
+})
+
+-- Programming Languages
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "rust" },
+  command = "setlocal textwidth=100 shiftwidth=4 tabstop=4 expandtab softtabstop=4",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "go" },
+  command = "setlocal noexpandtab tabstop=4 shiftwidth=4",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "c", "cpp", "h", "hpp" },
+  command = "setlocal shiftwidth=4 tabstop=4 expandtab softtabstop=4",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "java" },
+  command = "setlocal shiftwidth=4 tabstop=4 expandtab softtabstop=4",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "ruby" },
+  command = "setlocal shiftwidth=2 tabstop=2 expandtab softtabstop=2",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "php" },
+  command = "setlocal shiftwidth=4 tabstop=4 expandtab softtabstop=4",
+})
+
+-- Restore cursor position when opening file
+vim.api.nvim_create_autocmd("BufReadPost", {
+  callback = function()
+    local last_pos = vim.fn.line("'\"")
+    if last_pos > 1 and last_pos <= vim.fn.line("$") then
+      vim.api.nvim_win_set_cursor(0, { last_pos, 0 })
+    end
+  end,
+})
+
+-- Terminal settings
+vim.api.nvim_create_autocmd("TermOpen", {
+  callback = function()
+    vim.opt_local.number = false
+    vim.opt_local.relativenumber = false
+    vim.opt_local.signcolumn = "no"
   end,
 })
