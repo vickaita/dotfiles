c71d40772699cdd39faaefbbd60a67d23b017e49
Vick Aita <75151+vickaita@users.noreply.github.com>
2025-07-30 13:52:15 -0400

    feat: Custom lualine in neo-tree

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -26,18 +26,125 @@ return {
       },
     },
     sections = {
-      lualine_a = { "mode" },
-      lualine_b = { "branch", "diff", "diagnostics" },
-      lualine_c = { "filename" },
-      lualine_x = { "encoding", "fileformat", "filetype" },
-      lualine_y = { "progress" },
-      lualine_z = { "location" },
+      lualine_a = {
+        {
+          "mode",
+          cond = function()
+            return vim.bo.filetype ~= "neo-tree"
+          end,
+        },
+      },
+      lualine_b = {
+        "branch",
+        {
+          "diff",
+          cond = function()
+            return vim.bo.filetype ~= "neo-tree"
+          end,
+        },
+        {
+          "diagnostics",
+          cond = function()
+            return vim.bo.filetype ~= "neo-tree"
+          end,
+        },
+      },
+      lualine_c = {
+        {
+          "filename",
+          cond = function()
+            return vim.bo.filetype ~= "neo-tree"
+          end,
+        },
+      },
+      lualine_x = {
+        {
+          "encoding",
+          cond = function()
+            return vim.bo.filetype ~= "neo-tree"
+          end,
+        },
+        {
+          "fileformat",
+          cond = function()
+            return vim.bo.filetype ~= "neo-tree"
+          end,
+        },
+        {
+          "filetype",
+          cond = function()
+            return vim.bo.filetype ~= "neo-tree"
+          end,
+        },
+      },
+      lualine_y = {
+        {
+          "progress",
+          cond = function()
+            return vim.bo.filetype ~= "neo-tree"
+          end,
+        },
+      },
+      lualine_z = {
+        {
+          "location",
+          cond = function()
+            return vim.bo.filetype ~= "neo-tree"
+          end,
+        },
+        {
+          function()
+            local cwd = vim.fn.getcwd()
+            if vim.fn.filereadable(cwd .. "/package.json") == 1 then
+              return " JavaScript"
+            elseif vim.fn.filereadable(cwd .. "/Cargo.toml") == 1 then
+              return " Rust"
+            elseif vim.fn.filereadable(cwd .. "/go.mod") == 1 then
+              return " Go"
+            elseif
+              vim.fn.filereadable(cwd .. "/pyproject.toml") == 1
+              or vim.fn.filereadable(cwd .. "/requirements.txt") == 1
+            then
+              return " Python"
+            elseif vim.fn.filereadable(cwd .. "/Gemfile") == 1 then
+              return " Ruby"
+            elseif vim.fn.filereadable(cwd .. "/pom.xml") == 1 or vim.fn.filereadable(cwd .. "/build.gradle") == 1 then
+              return " Java"
+            end
+            return " Directory"
+          end,
+          cond = function()
+            return vim.bo.filetype == "neo-tree"
+          end,
+        },
+      },
     },
     inactive_sections = {
       lualine_a = {},
-      lualine_b = {},
-      lualine_c = { "filename" },
-      lualine_x = { "location" },
+      lualine_b = {
+        {
+          "branch",
+          cond = function()
+            return vim.bo.filetype == "neo-tree"
+          end,
+        },
+      },
+      lualine_c = {
+        {
+          "filename",
+          cond = function()
+            return vim.bo.filetype ~= "neo-tree"
+          end,
+        },
+      },
+      lualine_x = {
+        {
+          "location",
+          cond = function()
+            return vim.bo.filetype ~= "neo-tree"
+          end,
+        },
+      },
       lualine_y = {},
       lualine_z = {},
     },
