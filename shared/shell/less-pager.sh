c7973c2d278d4087d81cc58ff89ad4e97c795263
Vick Aita <75151+vickaita@users.noreply.github.com>
2025-08-18 12:31:06 -0400

    Reorder checks for lesspipe
    New order prioritizes homebrew install

--- shared/shell/less-pager.sh
+++ shared/shell/less-pager.sh
@@ -2,16 +2,16 @@
 export PAGER="less -R"
 
 # make less more friendly for non-text input files, see lesspipe(1)
-# Check if 'lesspipe' is available as a command in PATH (most common on Linux)
-if command -v lesspipe >/dev/null 2>&1; then
-  eval "$(SHELL=/bin/sh lesspipe)"
 # Check for lesspipe.sh installed by Homebrew on Apple Silicon Macs
-elif [ -x /opt/homebrew/bin/lesspipe.sh ]; then
-  eval "$(SHELL=/bin/sh /opt/homebrew/bin/lesspipe.sh)"
+if [ -x /opt/homebrew/bin/lesspipe.sh ]; then
+    eval "$(SHELL=/bin/sh /opt/homebrew/bin/lesspipe.sh)"
 # Check for lesspipe.sh installed by Homebrew on Intel Macs
 elif [ -x /usr/local/bin/lesspipe.sh ]; then
-  eval "$(SHELL=/bin/sh /usr/local/bin/lesspipe.sh)"
+    eval "$(SHELL=/bin/sh /usr/local/bin/lesspipe.sh)"
+# Check if 'lesspipe' is available as a command in PATH (most common on Linux)
+elif command -v lesspipe >/dev/null 2>&1; then
+    eval "$(SHELL=/bin/sh lesspipe)"
 # Check for lesspipe.sh in the standard Linux location
 elif [ -x /usr/bin/lesspipe.sh ]; then
-  eval "$(SHELL=/bin/sh /usr/bin/lesspipe.sh)"
+    eval "$(SHELL=/bin/sh /usr/bin/lesspipe.sh)"
 fi
