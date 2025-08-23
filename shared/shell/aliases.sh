9a11274104d9e2f360a0890b3788cfdbe276895e
Vick Aita <75151+vickaita@users.noreply.github.com>
2025-08-22 22:28:37 -0400

    Add some shell aliases

--- shared/shell/aliases.sh
+++ shared/shell/aliases.sh
@@ -2,7 +2,17 @@
 
 # Common aliases shared between bash and zsh
 
-# ls aliases
-alias ll='ls -alF'
-alias la='ls -A'
-alias l='ls -CF'
+# ls aliases with eza fallback
+if command -v eza >/dev/null 2>&1; then
+    alias ll='eza -la --icons --group-directories-first --git'
+    alias la='eza -a --icons --group-directories-first'
+    alias l='eza --icons --group-directories-first'
+else
+    alias ll='ls -alF'
+    alias la='ls -A'
+    alias l='ls -CF'
+fi
+
+# eza aliases
+alias lz='eza --icons --group-directories-first'
+alias lzl='eza -la --icons --group-directories-first --git'
