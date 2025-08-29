2447abd45710baa9e5a995a6645ae9f5f47e1b12
Vick Aita <75151+vickaita@users.noreply.github.com>
2025-08-29 16:07:35 -0400

    Don't open Atuin ui on up-arrow

--- shared/shell/atuin.sh
+++ shared/shell/atuin.sh
@@ -1,6 +1,6 @@
 # Atuin - magical shell history
 # Initialize atuin for the current shell if available
 if command -v atuin &>/dev/null; then
-    eval "$(atuin init "$CURRENT_SHELL")"
+    eval "$(atuin init "$CURRENT_SHELL" --disable-up-arrow)"
 fi
 
