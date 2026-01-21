a373e27a270abae44af50ba520e71be2ad42e352
Vick Aita <75151+vickaita@users.noreply.github.com>
2026-01-21 09:45:28 -0500

    Update python formatting order

--- nvim/.config/nvim/lua/plugins/python.lua
+++ nvim/.config/nvim/lua/plugins/python.lua
@@ -6,7 +6,7 @@ return {
   {
     "stevearc/conform.nvim",
     opts = {
-      formatters_by_ft = { python = { "ruff_organize_imports", "ruff_format" } },
+      formatters_by_ft = { python = { "ruff_fix", "ruff_organize_imports", "ruff_format" } },
     },
   },
   {
