a4ef5e260a8ce5e2bf108e4963c74297d3565a25
Vick Aita <75151+vickaita@users.noreply.github.com>
2026-01-21 09:42:09 -0500

    Update python formatting

--- nvim/.config/nvim/lua/plugins/python.lua
+++ nvim/.config/nvim/lua/plugins/python.lua
@@ -6,17 +6,14 @@ return {
   {
     "stevearc/conform.nvim",
     opts = {
-      formatters_by_ft = { python = { "ruff_fix", "isort", "black" } },
+      formatters_by_ft = { python = { "ruff_organize_imports", "ruff_format" } },
     },
   },
   {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
-        "black",
-        "isort",
         "ruff",
-        "ruff_fix",
         "pyrefly",
       },
     },
