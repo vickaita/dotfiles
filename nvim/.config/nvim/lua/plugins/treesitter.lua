3c19e7c571f5e6477c914f12a9e938d087b2ba54
Vick Aita <75151+vickaita@users.noreply.github.com>
2026-05-15 15:14:56 -0400

    fix(nvim): disable treesitter-context on nvim 0.12+
    TSNode:range() was removed in nvim 0.12, causing errors in markdown
    files. Guard the plugin until nvim-treesitter-context is updated.
    
    Refs: https://github.com/nvim-treesitter/nvim-treesitter-context/issues/670

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -120,6 +120,8 @@ return {
   },
   {
     "nvim-treesitter/nvim-treesitter-context",
+    -- TSNode:range() was removed in nvim 0.12; disable until the plugin catches up
+    enabled = vim.fn.has("nvim-0.12") == 0,
     opts = {
       enable = true,
       max_lines = 0,
